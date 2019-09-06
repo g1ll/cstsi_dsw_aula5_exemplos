5b1cbfbd17cb616c40bda1c3e821c28efbd9c42c
Gill <gillvelleda@gmail.com>
2019-09-06 17:20:32 -0300

    teste

--- listar.js
+++ listar.js
@@ -2,16 +2,19 @@
 const fetch = require('node-fetch');
 const apiUrl = 'http://g1ll.000webhostapp.com/ajaxphp/consulta.php';
 console.clear()
-console.log(`Recebendo dados de ${apiUrl}`)
+console.log(`Conectando a ${apiUrl}`)
+
+//fetch(url).then(resposta).then(data)
 fetch(apiUrl)
     .then(response => {
         return response.json();
     }).then(data => {
+        console.log('Recebendo dados!');
         // console.log(data);
         if (data) {
             console.table(data)
         }
     })
     .catch(error => {
-        console.log(`Erro:${error.message}`)
+        console.log(`Erro ao conectar:\n\n${error.message}`)
     });
