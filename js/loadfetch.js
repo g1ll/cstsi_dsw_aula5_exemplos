4ad8d79067fe77bb8b35d90decbf9b9b457f27a9
Gill <gillvelleda@gmail.com>
2019-09-13 10:29:10 -0300

    correção

--- js/loadfetch.js
+++ js/loadfetch.js
@@ -2,31 +2,32 @@
 //const fetch = require('node-fetch');
 
 console.clear();
+// const urlapi = "http://g1ll.000webhostapp.com/ajaxphp/consulta.php"
+const urlapi = "http://localhost/2018/cpw2/ajaxphp_2018/consulta.php"
+  
 
 const loading = document.getElementById('loading');
 const divData = document.querySelector('#data');
 
 function showLoader() {
     document.querySelector('.container-load').style.display='block';
-    loading.style.visibility='visible';
     console.log(loading);
     loading.className = "show";
 }
-function hideLoader(){
+function hiddeLoader(){
     loading.className = "";
-    loading.style.visibility='hidden';
     document.querySelector('.container-load').style.display='none';
 }
 
 function fetchData(){
     divData.innerHTML ='';
     showLoader();
-fetch('http://g1ll.000webhostapp.com/ajaxphp/consulta.php')
+fetch(urlapi)
     .then(response => {
         return response.json()
     }).then(data => {
         if (data.length) {
-            hideLoader();
+            hiddeLoader();
             console.table(data)
             let table = '<table border=1>'
             data.forEach(obj => {
