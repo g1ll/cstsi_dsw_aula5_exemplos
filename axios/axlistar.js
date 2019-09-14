05d821a5fb817cf94f62db6072899cc70fccb0ac
g1ll <gillvelleda@gmail.com>
2019-09-14 03:58:42 -0300

    Aula 13: XHR, Promises, Fetch e Axios

--- axios/axlistar.js
+++ axios/axlistar.js
@@ -1,10 +1,10 @@
 const axios = require('axios');
  //const apiUrl = 'http://g1ll.000webhostapp.com/ajaxphp/consulta.php';
-const apiUrl = 'http://localhost/2018/cpw2/ajaxphp_2018/consulta.php';
-const apiUrl = 'http://localhost:9090/consulta.php';
+//const apiUrl = 'http://localhost/2018/cpw2/ajaxphp_2018/consulta.php';
+const apiUrl = 'http://localhost:9090/consulta.php';//testando servidor Builtin PHP -S localhost:9090
 console.clear()
 console.log(`Conectando a ${apiUrl}`)
-
+    //tentando configurar cors com serve php -S
     let opt = {proxy: {
     host: 'localhost',
     port: 9000}, crossdomain: true,mode:'cors'}
