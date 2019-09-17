f3e0650b7fe6a7e3593a89a17d490c54d11d92fc
Gill <gillvelleda@gmail.com>
2019-09-16 23:12:00 -0300

    axios listar

--- axios/axlistar.js
+++ axios/axlistar.js
@@ -4,11 +4,14 @@ const apiUrl = 'http://g1ll.000webhostapp.com/ajaxphp/consulta.php';
 //const apiUrl = 'http://localhost:9090/consulta.php';//testando servidor Builtin PHP -S localhost:9090
 console.clear()
 console.log(`Conectando a ${apiUrl}`)
+
     //tentando configurar cors com serve php -S
-    let opt = {proxy: {
-    host: 'localhost',
-    port: 9000}, crossdomain: true,mode:'cors'}
-axios.get(apiUrl,opt)
+    //let opt = {proxy: {
+    //host: 'localhost',
+    //port: 9000}, crossdomain: true,mode:'cors'}
+    //axios.get(apiUrl,opt)...
+
+axios.get(apiUrl)
     .then(resp => {
         console.log('Recebendo dados!');
         if (resp.data) {
