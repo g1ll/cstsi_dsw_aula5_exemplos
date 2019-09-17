36babacc43a6883e3a8bbb57e992f6ab584c21d9
Gill <gillvelleda@gmail.com>
2019-09-16 23:00:25 -0300

    fix axios server urlapi

--- axios/axlistar.js
+++ axios/axlistar.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
- //const apiUrl = 'http://g1ll.000webhostapp.com/ajaxphp/consulta.php';
+const apiUrl = 'http://g1ll.000webhostapp.com/ajaxphp/consulta.php';
 //const apiUrl = 'http://localhost/2018/cpw2/ajaxphp_2018/consulta.php';
-const apiUrl = 'http://localhost:9090/consulta.php';//testando servidor Builtin PHP -S localhost:9090
+//const apiUrl = 'http://localhost:9090/consulta.php';//testando servidor Builtin PHP -S localhost:9090
 console.clear()
 console.log(`Conectando a ${apiUrl}`)
     //tentando configurar cors com serve php -S
