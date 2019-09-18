e1b870df1daef07464f83902c4257ec9361f0e0d
Gill <gillvelleda@gmail.com>
2019-09-18 13:21:58 -0300

    exemplos com busca e sql-injection

---
+++ axios/axbusca.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+//const apiUrl = 'http://g1ll.000webhostapp.com/ajaxphp/consulta.php';
+//const apiUrl = 'http://localhost/2018/cpw2/ajaxphp_2018/consulta.php';
+//const apiUrl = 'http://localhost:9090/consulta.php';//testando servidor Builtin PHP -S localhost:9090
+const apiUrl = 'http://localhost/consulta.php';//testando servidor Builtin PHP -S localhost:9090
+console.clear()
+console.log(`Conectando a ${apiUrl}`)
+
+    //tentando configurar cors com serve php -S
+    //let opt = {proxy: {
+    //host: 'localhost',
+    //port: 9000}, crossdomain: true,mode:'cors'}
+    //axios.get(apiUrl,opt)...
+
+axios.get(apiUrl)
+    .then(resp => {
+        console.log('Recebendo dados!');
+        if (resp.data) {
+            console.table(resp.data)
+        }
+    })
+    .catch(error => {
+        console.log(`Erro ao conectar:\n\n${error.message}`)
+        console.log(error)
+    });
--- imcapi/busca.php
+++ imcapi/busca.php
@@ -1,6 +1,6 @@
 <?php
 //header('Content-Type: charset=UTF-8');
-//header('Access-Control-Allow-Headers: *');
+header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Headers: Host, Connection, Accept, Authorization, Content-Type, X-Requested-With, User-Agent, Referer, Methods');
@@ -8,18 +8,23 @@ header('Access-Control-Allow-Origin: *');
 @$conn = mysqli_connect('localhost', 'id2581340_1mc', '1mc@g1ll', 'id2581340_imc');
 if(!$conn)
     $conn = mysqli_connect('localhost', 'root', 'r00t', 'imcApi');
-$sql = "SELECT * FROM imc ORDER BY id DESC Limit 10"; //Limita até as primeiras cinco posições
 
+$busca = (isset($_GET['busca']))?"WHERE name LIKE '%$_GET[busca]%'":"";
+
+$sql = "SELECT * FROM imc $busca ORDER BY id DESC Limit 10"; //Limita até as primeiras cinco posições
+//var_dump($sql);die;
 $result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) { 
- 
+if ($result&&mysqli_num_rows($result) > 0) { 
     $jsonData = mysqli_fetch_all($result,MYSQLI_ASSOC);
-   sleep(3);
+   //sleep(3);
+   echo"<pre>";
+   var_dump($jsonData);
+   echo"/<pre>";die;
    echo json_encode($jsonData);
     mysqli_free_result($result);
     mysqli_close($conn); 
 }else{
+    echo mysqli_error($conn);
     echo json_encode([]);
 }
 ?>
