010557680202f88f220bd9a5e8765269dc92aae7
g1ll <gillvelleda@gmail.com>
2019-11-14 00:25:25 -0300

    Exemplos de Upload de Arquivos

---
+++ imcapi/listarfoto.php
@@ -0,0 +1,38 @@
+<?php 
+
+    @$conn = mysqli_connect('localhost', 'id2581340_1mc', '1mc@g1ll', 'id2581340_imc');
+if(!$conn)
+    $conn = mysqli_connect('localhost', 'root', 'r00t', 'imcApi');
+$sql = "SELECT id,name,altura,peso,imc,foto.nome FROM imc LEFT JOIN foto ON foto.idImc = imc.id ORDER BY id DESC Limit 5"; //Limita até as primeiras cinco posições
+
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) { 
+ 
+    $rows = mysqli_fetch_all($result,MYSQLI_ASSOC);
+   //sleep(3);
+//    $jsonData = null;
+//    var_dump($jsonData);
+//    die;
+
+    // $jsonData = array_map(function($row){
+    //     if($row['nome']==null)
+    //         $row['nome'] = 'default.jpg';
+    //     return $row;
+    // },$rows);
+
+    foreach($rows as $row){
+        if($row['nome']==null)
+            $row['nome'] = 'default.jpg';
+        $jsonData[] = $row;
+    }
+
+    echo json_encode($jsonData);
+    
+    mysqli_free_result($result);
+    mysqli_close($conn); 
+}else{
+    echo json_encode([]);
+}
+
+?>
---
+++ js/listarimcfoto.js
@@ -0,0 +1,31 @@
+const urlapi = "http://localhost/cstsi_dsw_aula5_exemplos/imcapi/";
+const axiosApi = axios.create(
+    {   baseURL:urlapi,
+        headers:{'Content-Type':'application/json'}}
+        );
+
+async function carregaDados(){
+    const resp = await axiosApi.get('listarfoto.php');
+    const list = resp.data;
+    console.table(list);
+    mostraTabela(list,'dados');
+}
+
+function mostraTabela(dados,elid) {
+        div = document.getElementById(elid);
+        div.innerHTML = '';
+        let table = '<table border=1>'
+        dados.forEach(obj => {
+            console.log(obj)
+            table += '<tr>'
+            Object.entries(obj).map(([key, value]) => {
+                if(key==='nome'){
+                    table += `<td><img src='http://localhost/cstsi_dsw_aula5_exemplos/imcapi/upfile/${value}' width=100 height=100></img></td>`
+                }else{
+                    table += `<td>${value}</td>`
+                }
+            });
+            table += '</tr>'
+        });
+        div.innerHTML += table + '</table>';
+    }
