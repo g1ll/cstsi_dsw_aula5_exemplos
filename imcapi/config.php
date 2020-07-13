0d45e4bd62e8ba69ad6bbcddb7272bfa92a6f7a2
g1ll <gillvelleda@gmail.com>
2020-07-13 17:48:44 -0300

    fix connect imcapi

---
+++ imcapi/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost='localhost';
+$dbname='id14334931_imc';
+$dbuser='id14334931_g1llimc';
+$dbpass='I9TyB%m*%r5dE(aC';
+
--- imcapi/connect.php
+++ imcapi/connect.php
@@ -1,6 +1,6 @@
 <?php
 include "config.php";
-$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 if (!$conn) {
 	$conn = mysqli_connect('localhost', 'root', 'r00t', 'imcApi');
 	if(!$conn){
